05bcc70324ab566d06c0792216cb9957dcd19f63
Arley Santana <arleysantana@Arleys-iMac.local>
2024-08-16 21:36:36 -0300

    03 - Configurando Theme Light/Dark

--- app/src/main/java/br/com/hellodev/moviestreaming/MainActivity.kt
+++ app/src/main/java/br/com/hellodev/moviestreaming/MainActivity.kt
@@ -2,6 +2,7 @@ package br.com.hellodev.moviestreaming
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
@@ -18,7 +19,12 @@ import br.com.hellodev.moviestreaming.ui.theme.MovieStreamingTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.light(
+                android.graphics.Color.TRANSPARENT,
+                android.graphics.Color.TRANSPARENT
+            )
+        )
         setContent {
             MovieStreamingTheme {
                 Scaffold(modifier = Modifier
